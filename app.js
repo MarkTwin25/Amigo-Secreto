278da005f56dbe8a1ceef1c3b7fe180da8b56264
MarkTwin25 <jesusmartin-25@ciencias.unam.mx>
2025-03-15 21:36:48 -0600

    App.js completado

--- app.js
+++ app.js
@@ -1,2 +1,46 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+let amigos = [];
+
+// Funcion para agregar amigos
+function agregarAmigo(){
+    var amigo = document.getElementById('amigo').value;
+    if(amigo == ''){
+        alert('Por favor, inserte un nombre');
+        return;
+    }
+    if(amigos.includes(amigo)){
+        alert('El amigo ya fue agregado');
+        return;
+    }
+    amigos.push(amigo);
+
+    // Limpia el input
+    document.getElementById('amigo').value = ''; 
+
+    // Actualiza la lista de amigos
+    actualizarLista();
+}
+
+// Actualizar lista de amigos
+function actualizarLista(){
+    let lista = '';
+    for(let i=0; i<amigos.length; i++){
+        lista += `<li>${amigos[i]}</li>`;
+    }
+    document.getElementById('listaAmigos').innerHTML = lista;
+}
+
+// Sortear amigo secreto
+function sortearAmigo(){
+    if(amigos.length <2){
+        alert('Por favor, agregue al menos 2 amigos');
+        return;
+    }
+
+    // generar indice aleatorio
+    indice = Math.floor(Math.random() * amigos.length);
+    amigoSecreto = amigos[indice];
+    document.getElementById('resultado').innerHTML = `El amigo secreto es: ${amigoSecreto}`;
+
+}
